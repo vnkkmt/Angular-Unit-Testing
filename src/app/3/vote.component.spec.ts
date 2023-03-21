a115237227fcf9912d644db18394a4474ebcd306
VinayakKamath <vinayakkamath@qburst.com>
2023-03-21 16:57:58 +0530

    tesing components

---
+++ src/app/3/vote.component.spec.ts
@@ -0,0 +1,24 @@
+import { VoteComponent } from './vote.component'; 
+
+describe('VoteComponent', () => {
+    let component:VoteComponent
+    beforeEach(()=>{
+        component = new VoteComponent();
+    });
+    
+afterAll(()=>{
+
+})
+
+  it('should increment totalVotes when upvoted', () => {
+    component.upVote();
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    component.downVote();
+    expect(component.totalVotes).toBe(-1);
+  });
+
+
+});
