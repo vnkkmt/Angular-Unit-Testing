5a5a1aeb7c9edeac049cd4f8de71ec9f9bdc0fc2
VinayakKamath <vinayakkamath@qburst.com>
2023-03-30 17:20:55 +0530

    Delete button with confirmation boxes

--- src/app/6/todo.component.spec.ts
+++ src/app/6/todo.component.spec.ts
@@ -72,6 +72,23 @@ describe('TodosComponent', () => {
     component.add();
     //console.log(component.todos)
     expect(component.message).toBe(error);
-  })
+  });
+
+  it('should call the server to delete a todo item if the user confirms', ()=>{
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(of());
+
+    component.delete(1);
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  
+  it('should NOT call the server to delete a todo item if the user cancels', ()=>{
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(of());
+
+    component.delete(1);
+    expect(spy).not.toHaveBeenCalledWith(1);
+  });
 
 });
